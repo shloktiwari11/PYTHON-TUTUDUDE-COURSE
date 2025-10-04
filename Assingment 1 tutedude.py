8ec8d3283bf5c20f72756076f5d99733eae8ba1a
Shlok Avadheshkumar Tiwari <151176272+shloktiwari11@users.noreply.github.com>
2025-10-04 16:00:00 +0530

    Add files via upload

---
+++ Assingment 1 tutedude.py
@@ -0,0 +1,9 @@
+# Get input from the user and convert it to an integer
+number1 = int(input("Enter the first number: "))
+number2 = int(input("Enter the second number: "))
+
+# Perform calculations and print the results
+print(f"Addition: {number1 + number2}")
+print(f"Subtraction: {number1 - number2}")
+print(f"Multiplication: {number1 * number2}")
+print(f"Division: {number1 / number2}")
